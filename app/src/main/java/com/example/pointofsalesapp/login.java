3b2afa39aa85bb0337acc696f69a58f71cb5bc8a
Salvador Sanchez Alvarez <salvador.s@nafsolutions.co>
2024-12-23 15:47:06 -0600

    CHanged the compile versions to 28 and changed the heap size to 3g

--- app/src/main/java/com/example/pointofsalesapp/login.java
+++ app/src/main/java/com/example/pointofsalesapp/login.java
@@ -1,6 +1,5 @@
 package com.example.pointofsalesapp;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +7,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class login extends AppCompatActivity {
 
     EditText ed1,ed2;
     Button b1,b2;
@@ -16,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
 
 
         ed1 = findViewById(R.id.user);
